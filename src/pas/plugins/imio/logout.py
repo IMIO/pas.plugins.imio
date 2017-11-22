f24edb1ab23f4025343e1d52ae151d777727e92c
Benoît Suttor <bsuttor@imio.be>
2017-11-22 16:07:49 +0100

    Add logout event handler

---
+++ src/pas/plugins/imio/logout.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+def logout_from_authentic(event):
+    """
+    Logout event handler.
+
+    When user explicitly logs out from the Logout menu, logout from SSO too
+    """
+    pass

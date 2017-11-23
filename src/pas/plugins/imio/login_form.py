aa057319df0b5a98321465c194ae6c4145b256f7
Benoît Suttor <bsuttor@imio.be>
2017-11-23 11:07:02 +0100

    Lint

--- src/pas/plugins/imio/login_form.py
+++ src/pas/plugins/imio/login_form.py
@@ -1,12 +1,13 @@
-from Products.Five import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
+# -*- coding: utf-8 -*-
 from plone import api
+from Products.Five import BrowserView
+
 
 class LoginFormView(BrowserView):
 
     def __call__(self):
-        """"""
+        """Redirect login to authentic"""
         response = self.request.response
-        response.redirect('{0}/authomatic-handler/authentic'.format(api.portal.get().absolute_url()))
-
+        response.redirect('{0}/authomatic-handler/authentic'.format(
+            api.portal.get().absolute_url())
+        )

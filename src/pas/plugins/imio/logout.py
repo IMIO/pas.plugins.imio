5c4cb98059c56e153edabd62cbe82211947e069d
Benoît Suttor <bsuttor@imio.be>
2018-03-29 14:02:51 +0200

    Logout from plone before authentic

--- src/pas/plugins/imio/logout.py
+++ src/pas/plugins/imio/logout.py
@@ -9,7 +9,10 @@ class ImioLogoutFormView(BrowserView):
     def __call__(self):
         """Redirect login to authentic"""
 
-        # TODO: change personnal tool url
+        mt = api.portal.get_tool('portal_membership')
+        mt.logoutUser(self.request)
+        transaction_note('Logged out')
+
         authentic_hostname = api.portal.get_registry_record(
             'pas.plugins.imio.authentic_hostname', default=False)
         if not authentic_hostname:
@@ -18,8 +21,6 @@ class ImioLogoutFormView(BrowserView):
             authentic_hostname,
             api.portal.get().absolute_url()
         )
-        mt = api.portal.get_tool('portal_membership')
-        mt.logoutUser(self.request)
-        transaction_note('Logged out')
+
         response = self.request.response
         response.redirect(authentic_logout_url)

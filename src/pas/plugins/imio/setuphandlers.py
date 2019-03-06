87b40a11055727cf80819fcc35642de40bee78a4
Benoit Suttor <bsuttor@imio.be>
2019-03-06 11:26:46 +0100

    Add external url for login on plone 5 too

--- src/pas/plugins/imio/setuphandlers.py
+++ src/pas/plugins/imio/setuphandlers.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+from pkg_resources import parse_version
 from plone import api
+from plone.api import env
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+
+
+HAS_PLONE5 = parse_version(env.plone_version()) >= parse_version('5.0b2')
 
 
 @implementer(INonInstallable)
@@ -16,15 +21,35 @@ class HiddenProfiles(object):
 
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
-    site_properties = api.portal.get_tool('portal_properties').site_properties
-    site_properties.external_login_url = u'imio_login'
-    site_properties.external_logout_url = u'imio_logout'
+    if HAS_PLONE5:
+        api.portal.set_registry_record(
+            'plone.external_login_url',
+            u'imio_login'
+        )
+        api.portal.set_registry_record(
+            'plone.external_logout_url',
+            u'imio_logout'
+        )
+    else:
+        portal_properties = api.portal.get_tool('portal_properties')
+        site_properties = portal_properties.site_properties
+        site_properties.external_login_url = u'imio_login'
+        site_properties.external_logout_url = u'imio_logout'
 
 
 def uninstall(context):
     """Uninstall script"""
-    # Do something at the end of the uninstallation of this package.
-    site_properties = api.portal.get_tool('portal_properties').site_properties
-    site_properties.external_login_url = ''
-    site_properties.external_logout_url = ''
+    if HAS_PLONE5:
+        api.portal.set_registry_record(
+            'plone.external_login_url',
+            u''
+        )
+        api.portal.set_registry_record(
+            'plone.external_logout_url',
+            u''
+        )
+    else:
+        portal_properties = api.portal.get_tool('portal_properties')
+        site_properties = portal_properties.site_properties
+        site_properties.external_login_url = ''
+        site_properties.external_logout_url = ''

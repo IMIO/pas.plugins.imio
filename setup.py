234c7a2488b749bf68130610928b67bdfe697ef5
Benoit Suttor <bsuttor@imio.be>
2019-04-26 14:38:05 +0200

    Back to development: 1.0a5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='pas.plugins.imio',
-    version='1.0a4',
+    version='1.0a5.dev0',
     description="Pas plugin use to connect to auth.imio.be",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

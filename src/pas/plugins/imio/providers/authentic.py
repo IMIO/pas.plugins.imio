34c565507a2432157fc49c62bc169db6e18ea5a6
Benoit Suttor <bsuttor@imio.be>
2019-05-15 14:10:24 +0200

    providers/authentic.py is now python black compliant

--- src/pas/plugins/imio/providers/authentic.py
+++ src/pas/plugins/imio/providers/authentic.py
@@ -14,66 +14,67 @@ from authomatic.providers.oauth2 import OAuth2
 import jwt
 
 
-__all__ = ['Authentic']
+__all__ = ["Authentic"]
 
 
 class Authentic(OAuth2):
     """
     """
+
     @property
     def base_url(self):
         authentic_hostname = self.settings.config[self.name].get("hostname")
-        return 'https://{0}'.format(authentic_hostname)
+        return "https://{0}".format(authentic_hostname)
 
     @property
     def user_authorization_url(self):
-        return '{0}/idp/oidc/authorize/'.format(self.base_url)
+        return "{0}/idp/oidc/authorize/".format(self.base_url)
 
     @property
     def access_token_url(self):
-        return '{0}/idp/oidc/token/'.format(self.base_url)
+        return "{0}/idp/oidc/token/".format(self.base_url)
 
     @property
     def user_info_url(self):
-        return '{0}/idp/oidc/user_info/'.format(self.base_url)
+        return "{0}/idp/oidc/user_info/".format(self.base_url)
 
     @property
     def user_api_url(self):
-        return '{0}/api/users/'.format(self.base_url)
+        return "{0}/api/users/".format(self.base_url)
 
     @property
     def user_info_scope(self):
-        return ['openid', 'email', 'profile', 'roles']
+        return ["openid", "email", "profile", "roles"]
 
     @staticmethod
     def _x_user_parser(user, data):
-        encoded = data.get('id_token')
+        encoded = data.get("id_token")
         if encoded:
             payload_data = jwt.decode(
                 encoded,
-                algorithms=['RS256'],
-                options={'verify_signature': False, 'verify_aud': False}
+                algorithms=["RS256"],
+                options={"verify_signature": False, "verify_aud": False},
             )
-            if 'sub' in payload_data.keys():
-                user.id = user.username = payload_data.get('sub')
-        if 'sub' in data.keys():
+            if "sub" in payload_data.keys():
+                user.id = user.username = payload_data.get("sub")
+        if "sub" in data.keys():
             # user.id = data.get('sub')
             # user.id = data.get('email')
-            user.id = data.get('preferred_username')
+            user.id = data.get("preferred_username")
             user.username = user.id
-            user.first_name = data.get('given_name')
-            user.last_name = data.get('family_name')
-            fullname = u'{0} {1}'.format(user.first_name, user.last_name)
+            user.first_name = data.get("given_name")
+            user.last_name = data.get("family_name")
+            fullname = u"{0} {1}".format(user.first_name, user.last_name)
             if not fullname.strip():
                 user.name = user.id
                 user.fullname = user.id
             else:
                 user.name = fullname
                 user.fullname = fullname
-            roles = data.get('roles', [])
+            roles = data.get("roles", [])
             if len(roles) > 0:
-                if roles[0].get('name') == 'IASmartWeb':
-                    user.roles = ['Manager']
+                if roles[0] == "IASmartWeb":
+                    user.roles = ["Manager"]
         return user
 
 

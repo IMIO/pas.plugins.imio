22bcae71df8b765adfbd6ebc9a84597d9b2b6f47
Benoit Suttor <bsuttor@imio.be>
2020-03-27 09:36:07 +0100

    Improve next_url

--- src/pas/plugins/imio/login.py
+++ src/pas/plugins/imio/login.py
@@ -8,11 +8,10 @@ class ImioLoginFormView(BrowserView):
         """Redirect login to authentic"""
         response = self.request.response
         next_url = self.request.get("HTTP_REFERER", self.context.absolute_url())
-        if next_url:
+        url = "{0}/authentic-handler".format(api.portal.get().absolute_url())
+        if next_url and next_url != api.portal.get().absolute_url():
             next_url = next_url.replace(api.portal.get().absolute_url(), "")
             url = "{0}/authentic-handler/?next_url={1}".format(
                 api.portal.get().absolute_url(), next_url
             )
-        else:
-            url = "{0}/authentic-handler".format(api.portal.get().absolute_url())
         response.redirect(url)

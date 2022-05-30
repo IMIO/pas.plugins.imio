9843b6eb4f15ca99c886d2e66efa6a84ebaa750f
Laurent Lasudry <lasudry@gmail.com>
2022-05-30 14:53:38 +0200

    Add missing classifier for Plone 4.3

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",

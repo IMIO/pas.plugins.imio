3f2100dab4118871461c994f34caf98b14a7cdcc
Benoît Suttor <ben.suttor@gmail.com>
2025-07-15 09:15:21 +0200

    Back to development: 2.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="pas.plugins.imio",
-    version="2.1.1",
+    version="2.1.2.dev0",
     description="Pas plugin use to connect to auth.imio.be",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
